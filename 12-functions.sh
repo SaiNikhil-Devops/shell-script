97d4b12e3216d19b109ddab1815fabbbb1c597c8
thoutamsainikhil <thoutamsainikhils22@gmail.com>
2026-02-15 23:48:55 -0500

    adding new files

---
+++ 12-functions.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+    echo "Please run this script with root user access"
+    exit 1
+    fi
+VALIDATE(){
+    if [ $1 -ne 0 ]; then 
+    echo "$2.. Failure"
+    exit 1
+    else
+        echo "$2.. Success"
+    fi
+}
+
+
+dnf install nginx -y
+VALIDATE $? " Installing Nginx"
+
+dnf install mysql -y
+VALIDATE $? " Insatlling MySQL"
+
+dnf install nodejs -y
+VALIDATE $? "Installing Nodejs

a27224b16ac706c3321d9ebda2bef3bb3a7f152a
thoutamsainikhil <thoutamsainikhils22@gmail.com>
2026-02-16 00:14:50 -0500

    adding 14 14 16

---
+++ 15-loops-2.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# echo 1
+# echo 2
+# echo 3
+
+# for i in {1..100}
+# do
+#     echo $i
+# done
+
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE="/var/log/shell-script/$0.log"
+
+if [ $USERID -ne 0 ]; then
+    echo "Please run this script with root user access" | tee -a $LOGS_FILE
+    exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo "$2 ... FAILURE" | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo "$2 ... SUCCESS" | tee -a $LOGS_FILE
+    fi
+}
+
+for package in $@ # sudo sh 14-loops.sh nginx mysql nodejs
+do
+    dnf list installed $package &>>$LOGS_FILE
+    if [ $? -ne 0 ]; then
+        echo "$package is not installed. Installing now..." | tee -a $LOGS_FILE
+        dnf install $package -y &>>$LOGS_FILE
+        VALIDATE $? "$package installation"
+    else
+        echo "$package is already installed. Skipping installation..." | tee -a $LOGS_FILE
+    fi
+    
+done
---
+++ 16-colors.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+# echo -e "$R Hello World $N, $Y I am learning Linux $N" 
+
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE="/var/log/shell-script/$0.log"
+
+if [ $USERID -ne 0 ]; then
+    echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
+    exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$R $2 ... FAILURE $N" | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo -e "$G $2 ... SUCCESS $N" | tee -a $LOGS_FILE
+    fi
+}
+
+for package in $@ # sudo sh 14-loops.sh nginx mysql nodejs
+do
+    dnf list installed $package &>>$LOGS_FILE
+    if [ $? -ne 0 ]; then
+        echo "$package is not installed. Installing now..." | tee -a $LOGS_FILE
+        dnf install $package -y &>>$LOGS_FILE
+        VALIDATE $? "$package installation"
+    else
+        echo -e "$Y $package is already installed. Skipping installation... $N" | tee -a $LOGS_FILE
+    fi
+    
+done
